bae80677df828dce66ee76d115740f424b7a5aee
EricJ15 <166385675+EricJ15@users.noreply.github.com>
2024-11-13 09:04:35 +0800

    Component Updates

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import './SignUp.css';
 
 export const SignUp = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // Prevent default form submission
+        alert('Account has been created successfully!');
+      };
+
   return (
     <div class="signup-container">
         <h1>Sign Up</h1>
-        <form>
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-            <input type="password" placeholder="Confirm Password" />
-            <button type="submit">Sign Up</button>
-        </form>
+        <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Username" required />
+        <input type="password" placeholder="Password" required />
+        <input type="password" placeholder="Confirm Password" required />
+        <button type="submit">Sign Up</button>
+      </form>
     </div>
   )
 }
